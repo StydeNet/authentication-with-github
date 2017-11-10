c087213a6cbb5f0aad8afd0356fb840b8f4e497b
Duilio Palacios <duilio@styde.net>
2017-11-10 11:43:56 +0000

    first commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+class LoginController extends Controller
+{
+    public function redirectToGitHub()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function handleGitHubCallback()
+    {
+        $githubUser = Socialite::driver('github')->user();
+
+        $appUser = User::where('email', $githubUser->getEmail())->first();
+
+        if ($appUser == null) {
+            $appUser = User::create([
+                'email' => $githubUser->getEmail(),
+                'name' => $githubUser->getName(),
+            ]);
+        }
+
+        auth()->login($appUser);
+
+        return redirect('/');
+    }
+}
